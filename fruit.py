c01c9aa752b21188e1639999ae650e5b19ba9955
Kyle Gibeault <kylegibeault@gmail.com>
2016-05-16 20:11:45 -0700

    Added code to visualize decision tree

--- fruit.py
+++ fruit.py
@@ -10,3 +10,16 @@ clf = clf.fit(features, labels)
 # Importing the training data to "fit" the model
 print clf.predict([[160,0]])
 # Predicting how the model will evaluate a new data input
+
+#Visualizing the decision tree created by the model
+
+from sklearn.externals.six import StringIO  
+import pydot 
+dot_data = StringIO() 
+tree.export_graphviz(clf, 
+		out_file=dot_data,
+		filled=True,rounded=True,
+		impurity=False
+		) 
+graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
+graph.write_svg("decisiontree.svg") 
