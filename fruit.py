70db5f7c7deea269195e04ad2bbc49ae4e6d4484
Kyle Gibeault <kylegibeault@gmail.com>
2016-05-16 20:13:36 -0700

    Changing comment syntax

--- fruit.py
+++ fruit.py
@@ -1,15 +1,21 @@
-from sklearn import tree
+
 # imports the decision tree module from the scikit-learn project
-features = [[140, 1],[130, 1],[150, 0],[170, 0]]
+from sklearn import tree
+
 # Stores training samples of data
-labels = [0,0,1,1]
+features = [[140, 1],[130, 1],[150, 0],[170, 0]]
+
 # stores the classification value of each of the data into an array
-clf = tree.DecisionTreeClassifier()
+labels = [0,0,1,1]
+
 # instantiating a decision tree classifier from the tree module into the variable clf
-clf = clf.fit(features, labels)
+clf = tree.DecisionTreeClassifier()
+
 # Importing the training data to "fit" the model
-print clf.predict([[160,0]])
+clf = clf.fit(features, labels)
+
 # Predicting how the model will evaluate a new data input
+print clf.predict([[160,0]])
 
 #Visualizing the decision tree created by the model
 
